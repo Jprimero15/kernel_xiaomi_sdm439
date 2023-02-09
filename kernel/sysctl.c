fd6aa555a3494f0e5033afd81c8a40e8d3789498
Jprimero15 <jprimero155@gmail.com>
2023-02-09 14:02:55 +0000

    Revert "sysctl: promote sched_migration_cost_ns out of CONFIG_SCHED_DEBUG"
    This reverts commit 5fb2355e98b74437a33359809d9062b30991b528.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -486,7 +486,6 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
-#ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_nr_migrate",
 		.data		= &sysctl_sched_nr_migrate,
@@ -509,7 +508,6 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
-#endif /* CONFIG_SCHED_DEBUG */
 #ifdef CONFIG_SCHEDSTATS
 	{
 		.procname	= "sched_schedstats",
